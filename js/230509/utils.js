34717c94cc87d511f728151f80dca743ac816949
Jeong Subin <water4360@naver.com>
2023-05-09 15:26:55 +0900

    230509 강의실에서(함수function, onmouseover(), onmouseleave())

--- js/230509/utils.js
+++ js/230509/utils.js
@@ -17,7 +17,6 @@ function total(x, y) {
 // 이름을 입력받고
 // 이름이 제대로 입력되지 않으면, 재입력받은 후
 // 제대로 입력되면 로그에 이름 출력, 경고창으로 이름+환영인사 출력
-
 function enterName() {
   const name = prompt("이름을 입력해주세요");
   if (name !== "" && name !== null) {
@@ -31,16 +30,26 @@ function enterName() {
 }
 
 function enterNum() {
+  //prompt를 통해 숫자를 입력받고,
+  //입력받은 숫자를 num에 저장합니다
   const num = prompt("1~100 사이의 숫자를 입력해주세요");
+
+  //num이 숫자이고 + 1~100 사이의 숫자라면 for문 출동!
   if (!isNaN(num) && 1 <= num && num <= 100) {
     for (let i = 1; i <= 100; i++) {
       console.log(i);
     }
     alert("1~100 사이의 숫자출력 완료!");
+    //숫자이긴 한데 범위만 벗어난거면
+  } else if (!(1 <= num && num <= 100)) {
+    alert("1부터 100 사이의 숫자여야 해요.");
+    enterNum();
+    //취소를 누르면
   } else if (num === null) {
     alert("입력을 취소했어요~");
+    //그 밖의 비정상적인 입력들은 경고+함수 재호출
   } else {
-    alert("다시 입력해주세요~");
+    alert("잘못 입력했어요~");
     enterNum();
   }
 }
